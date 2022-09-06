dba6a7cbbf7ce77b87f276453f86e08ffd0f763b
Prosthius-swin <100637247+Prosthius-swin@users.noreply.github.com>
2022-09-06 14:54:37 +1000

    2 files modified

--- models/Country.ts
+++ models/Country.ts
@@ -4,17 +4,35 @@ import { Medals } from './Medals.enum';
 export class Country {
   // TODO: Country requires a name attribute and a results attribute.
   // name is a string, results is an array of Result
+  name: string;
+  results: IResult[];
 
   // TODO: receives a name and initialises the results array to an empty array
-  constructor(name: string) {}
+  constructor(_name: string, _results: IResult[]) {
+    this.name = _name;
+    this.results = _results;
+  }
 
   // return the total number of medals
   totalMedals(): number {
     //TODO: complete
+    return this.results.length;
   }
 
   // given a medal type, return the amount of that type of medal
   totalMedalType(medal: Medals): number {
     // TODO:
+    let numMedals: number;
+
+    for (let i of this.results) {
+      if (i === 'G') {
+        numMedals++;
+      } else if (i === 'S') {
+        numMedals++;
+      } else {
+        numMedals++;
+      }
+    }
+    return numMedals;
   }
 }
--- models/IResult.ts
+++ models/IResult.ts
@@ -1,7 +1,9 @@
-import { Sports } from './Sports.enum'
-import { Medals } from './Medals.enum'
+import { Sports } from './Sports.enum';
+import { Medals } from './Medals.enum';
 
 export interface IResult {
   // Result has a sport and medal
   // TODO
+  sport: string;
+  medal: Medals;
 }
