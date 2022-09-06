0bff9794499f182e336becad238a624fdfaf259d
Prosthius-swin <100637247+Prosthius-swin@users.noreply.github.com>
2022-09-06 14:03:22 +1000

    2 files modified

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-
+import { IResult } from './models/IResult';
+import { Medals } from './models/Medals.enum';
+import { Country } from './models/Country';
 import './style.css';
 // TODO: required imports
-
 
 const countrySelect: HTMLSelectElement = <HTMLSelectElement>(
   document.getElementById('country-slt')
@@ -23,7 +24,7 @@ init();
 // This function sets up some display elements
 function init() {
   let count = 0;
-  for (let c in Countries) {
+  for (let c in countries) {
     if (isNaN(Number(c))) {
       let newOption: HTMLOptionElement = document.createElement('option');
       newOption.innerHTML = c;
@@ -36,9 +37,7 @@ function init() {
   //TODO: populate the Sport select
 
   //TODO: populate the Medal select
-
 }
-
 
 // This function adds a medal to the countries tally
 function addMedal() {
--- models/Country.ts
+++ models/Country.ts
@@ -1,24 +1,20 @@
-import { Result } from './Result';
+import { IResult } from './IResult';
 import { Medals } from './Medals.enum';
 
 export class Country {
   // TODO: Country requires a name attribute and a results attribute.
   // name is a string, results is an array of Result
-  
+
   // TODO: receives a name and initialises the results array to an empty array
-  constructor(name: string) {
-    
-  }
+  constructor(name: string) {}
 
   // return the total number of medals
   totalMedals(): number {
     //TODO: complete
   }
 
-
   // given a medal type, return the amount of that type of medal
   totalMedalType(medal: Medals): number {
     // TODO:
   }
-
 }
